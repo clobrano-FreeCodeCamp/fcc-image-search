f3be2c03ecd7b74572a88389af3c261b662e3ac5
Carlo Lobrano <c.lobrano@gmail.com>
2016-05-29 23:22:18 +0200

    Added feature image search

--- routes/image_search.js
+++ routes/image_search.js
@@ -1,8 +1,23 @@
-var router = require('express').Router();
+var express = require('express');
+var googleImages = require('../lib/custom-google-images');
+
+var router = express.Router();
+
+var CSE_ID = process.env.CSE_ID;
+var APP_key = process.env.APP_KEY;
+var client = googleImages(CSE_ID, APP_key);
 
 router.get('/:query', function(req, res) {
     var offset = req.param('offset');
-    res.send('You searched: ' + req.params.query + ' offset: ' + offset);
+    var query = req.params.query;
+
+    if (!query) {
+        res.send('Error: query is empty');
+    }
+
+    client.search(query, {'num': offset}).then(function(images) {
+        res.send(images);
+    });
 });
 
 module.exports = router;
